7cc5e050b2b48d08691c5548e75d871503afbc64
jiro.kirameki <freche@eagle.sophia.ac.jp>
2017-04-25 07:56:58 +0000

    addcolumns

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :Status, presence: true, length: { maximum: 10 }
 end
 
